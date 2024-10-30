f86535de93c3822195758718916318813d75deb2
Lucas <lucas.ibar.soy.henry@gmail.com>
2024-10-30 12:55:43 -0300

    categoria de items algoon y nylon de color add

--- src/components/Remito/FormRemito/ItemsSearchBar/NuevoItem/NuevoItem.js
+++ src/components/Remito/FormRemito/ItemsSearchBar/NuevoItem/NuevoItem.js
@@ -11,7 +11,7 @@ import NavBar from '../../../../Utils/NavBar';
 export default function NuevoItem() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const categorias =  ["costura", "algodon", "nylon", "lycra", "goma", "tarugo", "etiqueta", "bolsa", "percha", "ribbon", "caja", "cinta", "plantilla", "film", "consumibes(aceite y parafina)", "faja", "caballete"]
+  const categorias =  ["costura", "algodon", "algodon-color", "nylon", "nylon-color", "lycra", "goma", "tarugo", "etiqueta", "bolsa", "percha", "ribbon", "caja", "cinta", "plantilla", "film", "consumibes(aceite y parafina)", "faja", "caballete"]
   
   const proveedorSeleccionado = useSelector((state) => state.proveedorSeleccionado);
 
